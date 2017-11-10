644ab6b38075530337e40534bf12b033e503f3b8
suzhenhui <szh362680581@qq.com>
2017-11-10 19:05:05 +0800

    "完成详情页数据交互"

--- app/src/main/java/com/magicsu/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/magicsu/criminalintent/SingleFragmentActivity.java
@@ -18,6 +18,7 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
