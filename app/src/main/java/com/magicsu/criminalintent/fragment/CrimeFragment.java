e35eca061f6c9fc8558b658059c1ef4e3c2c087f
suzhenhui <362680581@qq.com>
2017-11-16 17:17:00 +0800

    "优化隐式intent, 用shareCompat代替原生intent"

--- app/src/main/java/com/magicsu/criminalintent/fragment/CrimeFragment.java
+++ app/src/main/java/com/magicsu/criminalintent/fragment/CrimeFragment.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -108,13 +109,20 @@ public class CrimeFragment extends Fragment {
 
         mReportButton = v.findViewById(R.id.crime_report);
         mReportButton.setOnClickListener(view -> {
-            Intent i = new Intent(Intent.ACTION_SEND);
-            i.setType("text/plain");
-            i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-            i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
+            ShareCompat.IntentBuilder intentBuilder = ShareCompat.IntentBuilder.from(getActivity());
+            intentBuilder.setType("text/plain")
+                    .setText(getCrimeReport())
+                    .setSubject(getString(R.string.crime_report_subject))
+                    .setChooserTitle(getString(R.string.send_report))
+                    .startChooser();
 
-            i = Intent.createChooser(i, getString(R.string.send_report));
-            startActivity(i);
+//            Intent i = new Intent(Intent.ACTION_SEND);
+//            i.setType("text/plain");
+//            i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+//            i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
+//
+//            i = Intent.createChooser(i, getString(R.string.send_report));
+    //        startActivity(i);
         });
 
         final Intent pickContact = new Intent(Intent.ACTION_PICK,
