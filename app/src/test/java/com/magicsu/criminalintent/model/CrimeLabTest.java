a1f4900c14be53e6a8997e8813239e6870ba17da
suzhenhui <362680581@qq.com>
2017-11-21 09:29:36 +0800

    "添加file path文件"

---
+++ app/src/test/java/com/magicsu/criminalintent/model/CrimeLabTest.java
@@ -0,0 +1,40 @@
+package com.magicsu.criminalintent.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admin on 2017/11/16.
+ */
+public class CrimeLabTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void getCrimes() throws Exception {
+    }
+
+    @Test
+    public void getCrime() throws Exception {
+    }
+
+    @Test
+    public void addCrime() throws Exception {
+    }
+
+    @Test
+    public void updateCrime() throws Exception {
+    }
+
+    @Test
+    public void removeCrime() throws Exception {
+    }
+
+}
